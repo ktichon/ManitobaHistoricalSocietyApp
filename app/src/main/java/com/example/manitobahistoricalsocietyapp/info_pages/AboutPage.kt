695732a687cfc3fa88b594bb4baf9abc6ef43578
ktichon <ktichon@icloud.com>
2024-09-19 09:12:47 -0500

    Removing Changes to see why the legend bottom window is acting funny

---
+++ app/src/main/java/com/example/manitobahistoricalsocietyapp/info_pages/AboutPage.kt
@@ -0,0 +1,15 @@
+package com.example.manitobahistoricalsocietyapp.info_pages
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.example.manitobahistoricalsocietyapp.R
+import com.example.manitobahistoricalsocietyapp.navigation.AboutDestination
+import com.example.manitobahistoricalsocietyapp.ui.theme.AppTheme
+
---
+++ app/src/main/java/com/example/manitobahistoricalsocietyapp/info_pages/InfoPageScaffolding.kt
@@ -0,0 +1,25 @@
+package com.example.manitobahistoricalsocietyapp.info_pages
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+
+import androidx.compose.material3.Scaffold
+
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.manitobahistoricalsocietyapp.R
+
+
+import com.example.manitobahistoricalsocietyapp.site_scaffolding.TopBarNavIcon
+
+
+
+
